84655f0458046fe37ab2239aede44c3c11cd3336
Dmitrii Kozlov <dmitrij.kozlov.h@gmail.com>
2021-12-15 17:05:51 +0000

    2b done

--- 2.c
+++ 2.c
@@ -2,16 +2,17 @@
 #include <string.h>
 
 typedef unsigned long long int uint64;
+typedef signed long long int int64;
 
 int main(int argc, char** argv)
 {
-    uint64 depth = 0, horizontal = 0;
+    int64 depth = 0, horizontal = 0, aim = 0;
     while (1)
     {
         char command[25];
-        uint64 value = 0;
+        int64 value = 0;
         
-        int result = scanf_s("%24s %llu", command, 10, &value);
+        int result = scanf_s("%24s %lld", command, 10, &value);
         // printf("Read %s %llu\n", command, value);
         if (result != 2 || result == EOF)
         {
@@ -21,22 +22,20 @@ int main(int argc, char** argv)
         if (strcmp(command, "forward") == 0)
         {
             horizontal += value;
-        } else if (strcmp(command, "down") == 0)
-        {
-            depth += value;
-        } else if (strcmp(command, "up") == 0)
-        {
-            if (depth < value)
+            depth += aim * value;
+            if (depth < 0)
             {
                 depth = 0;
             }
-            else
-            {
-                depth -= value;
-            }
+        } else if (strcmp(command, "down") == 0)
+        {
+            aim += value;
+        } else if (strcmp(command, "up") == 0)
+        {
+            aim -= value;
         }
 
-        printf("New values: %llu %lld\n", depth, horizontal);
+        printf("New values: %lld %lld / aim %lld \n", depth, horizontal, aim);
     }
     printf_s("%llu", depth * horizontal);
     return 0;
